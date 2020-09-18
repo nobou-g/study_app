96bca3d15b9cb6306b95217fc0078011c07a4ad4
nobou-g <61077726+nobou-g@users.noreply.github.com>
2020-09-18 22:40:13 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions: 'users/sessions'   
   }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'events#index'
   resources :events do
     collection do
