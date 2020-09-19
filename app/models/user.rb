730aa1a621adf13e7455cb680f56aa4e504cba1d
nobou-g <61077726+nobou-g@users.noreply.github.com>
2020-09-19 23:37:11 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,6 @@ class User < ApplicationRecord
   has_many :messages
   has_many :events
   has_many :items
-  # has_many :relationships
-  # has_many :followings, through: :relationships, source: :follow
-  # has_many :reverse_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
-  # has_many :followers, through: :reverse_of_relationship, source: :user
   has_many :active_relationships, class_name: 'Relationship',
               foreign_key: 'user_id',
               dependent: :destroy
